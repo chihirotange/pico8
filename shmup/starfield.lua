4796ab32ded3f60f8f6c6be3e1407b5baebc4aad
ai-chan <ohanasakuiroha@gmail.com>
2023-12-01 11:31:36 +0700

    switch logic to object

--- shmup/starfield.lua
+++ shmup/starfield.lua
@@ -1,24 +1,19 @@
-function Starfield_Update()
-   for i=1,#stars_y do
-        local sy = stars_y[i]
-        if(sy<128) then
-            sy = sy + stars_sp[i]
-        else
-            sy = 0
+starfield = entity:new({
+    sc = 7,
+    speed = 0,
+    update = function(_ENV)
+        if y > 128 then
+            y = 0
         end
-        stars_y[i] = sy
-   end
-end
-
-function Starfield_Draw()
-    for i=1,#stars_x do
-        sp = stars_sp[i]
-        sc = 7
-        if (sp < 1) then
-            sc = 5 
-        elseif (sp < 1.5) then
+        y += speed
+    end,
+    draw = function(_ENV)
+        if (speed < 0.8) then
+            sc = 5
+        elseif (speed < 1.5) then
             sc = 6
         end
-        pset(stars_x[i], stars_y[i], sc)
+        pset(x, y, sc)
+        printh(draw_order)
     end
-end
+})
