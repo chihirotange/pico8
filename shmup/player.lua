6585716995a08b780c72f4bae770d9fdfae1eab4
ai-chan <ohanasakuiroha@gmail.com>
2023-12-07 13:01:40 +0700

    added score logic

--- shmup/player.lua
+++ shmup/player.lua
@@ -2,8 +2,13 @@ player = entity:new({
     is_abstract = false,
     max_lives = 4,
     current_lives = 4,
-    score = 1000,
+    score = 0,
     on_spaceship_collides_enemy = function(_ENV)
         current_lives -= 1
+    end,
+    on_bullet_collide_enemy = function(_ENV)
+        printh("in player")
+        score += 1000
+        printh(score)
     end
 })
